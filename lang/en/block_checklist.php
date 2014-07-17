db6e00b9dc0191dc30f2a09fa32394e140ffcf57
trevor-c <trevor.cunningham@uregina.ca>
2014-07-17 13:21:44 -0600

    bringing in updates from upstream

--- lang/en/block_checklist.php
+++ lang/en/block_checklist.php
@@ -1,8 +1,10 @@
 <?php
 
 $string['checklist'] = 'Checklist';
+$string['checklist:addinstance'] = 'Add Checklist block to My Moodle page';
+$string['checklist:myaddinstance'] = 'Add new Checklist block';
 $string['choosechecklist'] = 'Choose checklist';
-$string['choosegroup'] = 'Choose group';
+$string['choosegroup'] = 'Default group';
 $string['nochecklist'] = 'Please edit this block to select a checklist to display';
 $string['nochecklistplugin'] = 'You need to install the latest version of the checklist plugin for this block to work';
 $string['nousers'] = 'No users';
